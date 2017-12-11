d96fa898ca1a327b2c165adddb1f5fa42847d557
blackmiaool <blackmiaool@gmail.com>
2017-12-12 00:45:22 +0800

    update draw func

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ function fromArr(arr) {
     const cbList = [(node) => root = node];
 
     arr.forEach((val) => {
+        const node = new TreeNode(val);
+        const cb = cbList.shift();
+        cb(node);
         if (val === null) {
             return;
         }
 
-        const node = new TreeNode(val);
-        const cb = cbList.shift();
-
-        cb(node);
         cbList.push((n) => node.left = n);
         cbList.push((n) => node.right = n);
     });
@@ -46,12 +45,15 @@ function getMaxMin(root) {
     let max = -Infinity;
     let min = Infinity;
     function traverse(node) {
-        if (node.val > max) {
-            max = node.val;
+        if(node.val!==null){
+            if (node.val > max) {
+                max = node.val;
+            }
+            if (node.val < min) {
+                min = node.val;
+            }
         }
-        if (node.val < min) {
-            min = node.val;
-        }
+        
         if (node.left) {
             traverse(node.left);
         }
@@ -62,13 +64,58 @@ function getMaxMin(root) {
     traverse(root);
     return { max, min };
 }
+function val2str(val, width) {
+    if(val===null){
+        return ' '.repeat(width);
+    }
+    val=val+'';
+
+    const spaces=width-val.length;
+    if(spaces%2){        
+        val=' '.repeat((spaces-1)/2)+val+' '.repeat((spaces+1)/2);
+    }else {
+        val=' '.repeat(spaces/2)+val+' '.repeat(spaces/2);
+    }
+    return val;
+}
 function print(arr) {
     const tree = fromArr(arr);
-    const height = getHeight(tree);
+    console.log('tree', tree);
+    const treeHeight = getHeight(tree);
     const { min, max } = getMaxMin(tree);
+    
     const width = Math.max((min + '').length, (max + '').length);
+
+    const allNodes = [];
+    function traverse(node, height) {
+        height++;
+        const layerIndex = height - 1;
+        if (!allNodes[layerIndex]) {
+            allNodes[layerIndex] = [];
+        }
+        allNodes[layerIndex].push(val2str(node && node.val, width));
+        if (height >= treeHeight)
+            return;
+
+        traverse(node && node.left || null, height);
+        traverse(node && node.right || null, height);
+
+    }
+    traverse(tree, 0);
+    console.log('allNodes', allNodes);
+    drawAllNodes(allNodes);
 }
-print([1,2,2,3,4,4,3]);
+function drawAllNodes(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const line = arr[i];
+        console.log(line.join(' '));
+        // for(let j=0;j<line.length;j++){
+        //     const val=line[j];
+        //     console.log(val);
+        // }
+    }
+}
+print([1, 5, 2, 3, 4, null, 3]);
 if (typeof exports !== 'undefined') {
     module.exports = {
         fromArr,
